4036af0da3bbfbe34a1d143589c720b81f632e2e
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-07-25 21:03:44 +0530

    Create reducer.js

---
+++ src/redux/Login/reducer.js
@@ -0,0 +1,49 @@
+import { loadData, saveData } from "../../utils/localStorage";
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQ,
+  LOGIN_SUCCESS,
+  LOGOUT_PAGE,
+} from "./actiontype";
+
+let isAuth = loadData("auth") || false;
+const initstate = {
+  isAuth: isAuth,
+  isloading: false,
+  isError: false,
+  token: "",
+  username: "",
+};
+export const LoginReducer = (state = initstate, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case LOGIN_REQ:
+      return {
+        ...state,
+        isloading: true,
+      };
+    case LOGIN_SUCCESS:
+      saveData("auth", true);
+      return {
+        ...state,
+        isloading: false,
+        token: payload.token,
+        username: payload.username,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isloading: false,
+        isError: true,
+      };
+    case LOGOUT_PAGE:
+      saveData("auth", false);
+      return {
+        ...state,
+        isAuth: false,
+      };
+    default:
+      return state;
+  }
+};
