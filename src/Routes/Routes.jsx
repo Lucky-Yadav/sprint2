f1453d839228dccd77ab5a2df125d47a73da62c2
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-07-25 21:11:59 +0530

    Update Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "../components/Login";
 import { Home } from "./Home";
-import { Info } from "./Info";
+// import { Info } from "./Info";
 import { Navbar } from "./Navbar";
 import { PrivateRoute } from "./PrivateRoute";
 
@@ -15,8 +15,8 @@ function Routes() {
         <Route exact path="/">
           <Home />
         </Route>
-        <PrivateRoute exact path="/info">
-          <Info />
+        <PrivateRoute exact path="/">
+          {/* <Info /> */}
         </PrivateRoute>
 
         <Route exact path="/login">
